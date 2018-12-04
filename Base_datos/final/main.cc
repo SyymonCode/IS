3f0fc6e3235e7564f6a9f94d3f34bccdf6dd7fb3
Simón García Barea <i72gabas@uco.es>
2018-12-04 13:17:25 +0100

    Añadido el main

--- Base_datos/final/main.cc
+++ Base_datos/final/main.cc
@@ -1,1 +1,10 @@
 //Este es el archivo principal donde ira el programa
+//Incluid aqui las bibliotecas que sean necesarias
+#include <cstdio>
+#include <iostream>
+#include <string>
+int main(int argc, char const *argv[])
+{
+
+  return 0;
+}
